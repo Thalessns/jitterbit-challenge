13ae30a0829d66fe6f911ee9b521809df36a64eb
= <tthales08@gmail.com>
2026-03-07 16:18:51 -0300

    Adding endpoints, simple error handling and initial migrations

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const orderRouter = express.Router()
 const validate = require('../middleware/validator')
-const { orderRequest } = require('../schemas/order')
+const { orderRequest, orderUpdate } = require('../schemas/order')
 
-orderRouter.post(
-    "/", 
-    validate({ body: orderRequest }),
-    (req, res) => {
-    res.status(201).send('Order created successfully!')
+const { createOrder, listAllOrders, getOrder, updateOrder, deleteOrder, } = require('../service/order-service')
+
+orderRouter.post("/", validate({ body: orderRequest }), async (req, res) => {
+    const result = await createOrder(req.body)
+    res.status(201).json(result)
 })
 
-orderRouter.get(
-    "/", 
-    (req, res) => {
-    res.status(200).send('List of orders')
+orderRouter.get("/list", async (req, res) => {
+    const result = await listAllOrders()
+    res.status(200).json(result)
+})
+
+orderRouter.get("/:id", async (req, res) => {
+    const result = await getOrder(req.params.id)
+    res.status(200).json(result)
+})
+
+orderRouter.patch("/:id", validate({ body: orderUpdate }), async (req, res) => {
+    const result = await updateOrder(req.params.id, req.body)
+    res.status(200).json(result)
+})
+
+orderRouter.delete("/:id", async (req, res) => {
+    const result = await deleteOrder(req.params.id)
+    res.status(204).json(result)
 })
 
 module.exports = { orderRouter }
--- src/schemas/item.js
+++ src/schemas/item.js
@@ -1,5 +1,5 @@
 
-const item = {
+const itemRequest = {
     type: 'object',
     properties: {
         idItem: { type: 'string' , minLength: 1 },
@@ -10,4 +10,15 @@ const item = {
     required: ['idItem', 'quantidadeItem', 'valorItem']
 }
 
-module.exports = { item }
+const itemUpdate = {
+    type: 'object',
+    properties: {
+        idItem: { type: 'string' , minLength: 1 },
+        quantidadeItem: { type: ['integer', 'null'], minimum: 1, default: null },
+        valorItem: { type: ['number', 'null'], minimum: 0.01, default: null }
+    },
+    additionalProperties: false,
+    required: ["idItem"]
+}
+
+module.exports = { itemRequest, itemUpdate }
